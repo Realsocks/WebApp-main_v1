5e6bce4717497b1c4862040b1a9b698dac8a664a
Realsocks <kzeltov110@gmail.com>
2022-11-04 12:48:13 +0400

    Commit change liquibase

--- demo_fixed/src/main/java/com/example/demo/controlles/MOdul.java
+++ demo_fixed/src/main/java/com/example/demo/controlles/MOdul.java
@@ -3,11 +3,8 @@ package com.example.demo.controlles;
 import com.example.demo.Employees;
 import com.example.demo.repos.EmployeeRepo;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MOdul {
