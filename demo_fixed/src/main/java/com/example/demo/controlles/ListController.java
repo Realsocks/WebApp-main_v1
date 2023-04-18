76488784a091be340bd4228a871665b7b1bd9c7d
Realsocks <kzeltov110@gmail.com>
2023-04-18 18:38:51 +0400

    апдейт для курсача2

--- demo_fixed/src/main/java/com/example/demo/controlles/ListController.java
+++ demo_fixed/src/main/java/com/example/demo/controlles/ListController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controlles;
 
+import com.example.demo.Entitys.Employees;
 import com.example.demo.repos.EmployeeRepo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+
+
 
 @Controller
 public class ListController {
@@ -15,13 +18,20 @@ public class ListController {
         this.listrepo = listrepo;
 
     }
-    @PostMapping("/del")
+    /*@PostMapping("/del")
     public String deleteById(@RequestParam int id, HttpServletRequest request){
 
         listrepo.deleteById(id);
-        return "delete";
+        return "Employee";
 
+    }*/
+    @PostMapping( value = "/del/Employee/{id}")
+    @ResponseBody
+    public String Delete(@PathVariable(value="id") long id, @RequestParam(required = false) Integer Employees, Model model) {
+        Employees post = listrepo.findById((int) id).orElseThrow();
+        listrepo.deleteById((int) id);
 
+        return "";
 
     }
 
